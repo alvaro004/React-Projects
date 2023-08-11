e5c3c96277d398aa692aede02690998940ada283
Alvaro0004 <alvarollano04@gmail.com>
2023-08-10 22:34:48 -0400

    basic-react-form: printing elements in the view

--- Basic Projects/basic-react-form/src/App.js
+++ Basic Projects/basic-react-form/src/App.js
@@ -1,19 +1,31 @@
-const task = [
-  { id: 1, description: 'Take out the trash', status: false },
-  { id: 2, description: 'Study mandarin', status: false },
+const tasks = [
+  { id: 1, description: 'Take out the trash', deadline: '10-12-2023', status: false },
+  { id: 2, description: 'Study mandarin', deadline: '10-12-2023', status: false },
+  { id: 3, description: 'Eat something', deadline: '10-14-2023', status: true },
 ];
 
 export default function App() {
   return (
     <div>
       <h1>Task Creator</h1>
-      <form>
+      <form className="center">
         <textarea cols="30" placeholder="Your task"></textarea>
+        <input type="date" />
         <button>Send</button>
       </form>
-      <ul>
-        <li></li>
-      </ul>
+      {tasks.map((task) => {
+        return <Task id={task.id} description={task.description} deadline={task.deadline} status={task.status} />;
+      })}
     </div>
+  );
+}
+
+function Task(task) {
+  return (
+    <ul>
+      <li>Description: {task.description}</li>
+      <li>Deadline: {task.deadline}</li>
+      <li>Status: {task.status ? 'Done' : 'UnDone'}</li>
+    </ul>
   );
 }
