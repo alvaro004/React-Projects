93bc04ffe05f81b1b23ba80664f03024d833120e
Alvaro0004 <alvarollano04@gmail.com>
2023-08-10 23:03:22 -0400

    basic-react-form: Creating the newTask object based on the user imnput

--- Basic Projects/basic-react-form/src/App.js
+++ Basic Projects/basic-react-form/src/App.js
@@ -1,16 +1,27 @@
+import { useState } from 'react';
+
 const tasks = [
-  { id: 1, description: 'Take out the trash', deadline: '10-12-2023', status: false },
-  { id: 2, description: 'Study mandarin', deadline: '10-12-2023', status: false },
-  { id: 3, description: 'Eat something', deadline: '10-14-2023', status: true },
+  { id: 1, description: 'Take out the trash', deadline: '2023-08-11', status: false },
+  { id: 2, description: 'Study mandarin', deadline: '2023-08-12', status: false },
+  { id: 3, description: 'Eat something', deadline: '2023-08-13', status: true },
 ];
 
 export default function App() {
+  const [description, setDescription] = useState('');
+  const [deadline, setDeadline] = useState('');
+
+  function handleForm(e) {
+    e.preventDefault();
+    const newTask = { id: Date.now(), description, deadline, status: false };
+    console.log(newTask);
+  }
+
   return (
     <div>
       <h1>Task Creator</h1>
-      <form className="center">
-        <textarea cols="30" placeholder="Your task"></textarea>
-        <input type="date" />
+      <form className="center" onSubmit={handleForm}>
+        <textarea cols="30" placeholder="Your task" value={description} onChange={(event) => setDescription(event.target.value)}></textarea>
+        <input type="date" value={deadline} onChange={(event) => setDeadline(event.target.value)} required />
         <button>Send</button>
       </form>
       {tasks.map((task) => {
