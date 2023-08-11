22c3557f3a85a090d0203d88cbd25f01c6124941
Alvaro0004 <alvarollano04@gmail.com>
2023-08-11 01:49:37 -0400

    basic-react-form: Todo app ready with ChatGPT

--- Basic Projects/basic-react-form/src/App.js
+++ Basic Projects/basic-react-form/src/App.js
@@ -1,42 +1,51 @@
 import { useState } from 'react';
-
-const tasks = [
-  { id: 1, description: 'Take out the trash', deadline: '2023-08-11', status: false },
-  { id: 2, description: 'Study mandarin', deadline: '2023-08-12', status: false },
-  { id: 3, description: 'Eat something', deadline: '2023-08-13', status: true },
-];
+import './index.css';
 
 export default function App() {
-  const [description, setDescription] = useState('');
+  const [todos, setTodos] = useState([]);
+  const [newTodo, setNewTodo] = useState('');
   const [deadline, setDeadline] = useState('');
 
-  function handleForm(e) {
-    e.preventDefault();
-    const newTask = { id: Date.now(), description, deadline, status: false };
-    console.log(newTask);
-  }
+  const handleTodoChange = (e) => {
+    setNewTodo(e.target.value);
+  };
+
+  const handleDeadlineChange = (e) => {
+    setDeadline(e.target.value);
+  };
+
+  const handleAddTodo = () => {
+    if (newTodo.trim() === '' || deadline.trim() === '') return;
+
+    const newTodoItem = {
+      id: Date.now(),
+      title: newTodo,
+      deadline: deadline,
+    };
+
+    setTodos((prevTodos) => [...prevTodos, newTodoItem]);
+    setNewTodo('');
+    setDeadline('');
+  };
 
   return (
-    <div>
-      <h1>Task Creator</h1>
-      <form className="center" onSubmit={handleForm}>
-        <textarea cols="30" placeholder="Your task" value={description} onChange={(event) => setDescription(event.target.value)}></textarea>
-        <input type="date" value={deadline} onChange={(event) => setDeadline(event.target.value)} required />
-        <button>Send</button>
-      </form>
-      {tasks.map((task) => {
-        return <Task id={task.id} description={task.description} deadline={task.deadline} status={task.status} />;
-      })}
+    <div className="container">
+      <h1>Todo App</h1>
+      <div className="input-group">
+        <input type="text" placeholder="Todo" className="input" value={newTodo} onChange={handleTodoChange} />
+        <input type="date" className="input" value={deadline} onChange={handleDeadlineChange} />
+        <button className="add-button" onClick={handleAddTodo}>
+          Add Todo
+        </button>
+      </div>
+      <ul className="todo-list">
+        {todos.map((todo) => (
+          <li className="todo-item" key={todo.id}>
+            <span className="todo-title">{todo.title}</span>
+            <span className="deadline">{todo.deadline}</span>
+          </li>
+        ))}
+      </ul>
     </div>
-  );
-}
-
-function Task(task) {
-  return (
-    <ul>
-      <li>Description: {task.description}</li>
-      <li>Deadline: {task.deadline}</li>
-      <li>Status: {task.status ? 'Done' : 'UnDone'}</li>
-    </ul>
   );
 }
