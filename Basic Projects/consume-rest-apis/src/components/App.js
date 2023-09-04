ef2af29c0e8b607ab69f9394788beecef1860a88
Alvaro0004 <alvarollano04@gmail.com>
2023-09-04 18:35:33 -0400

    Consume-rest_apis: displaying data

--- Basic Projects/consume-rest-apis/src/components/App.js
+++ Basic Projects/consume-rest-apis/src/components/App.js
@@ -1,10 +1,31 @@
+import { useState, useEffect } from 'react';
+
 export default function App() {
-  fetch('https://jsonplaceholder.typicode.com/posts?_limit=10')
-    .then((response) => response.json())
-    .then((data) => console.log(data));
+  const [posts, setPosts] = useState([]);
+
+  useEffect(() => {
+    fetch('https://jsonplaceholder.typicode.com/posts?_limit=10')
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data);
+        setPosts(data);
+      })
+      .catch((err) => {
+        console.log(err.message);
+      });
+  }, []);
+
   return (
     <div className="App">
-      <div className="card"></div>
+      {posts.map((post) => {
+        return (
+          <div className="card" key={post.id}>
+            <h2>{post.title}</h2>
+            <p>{post.body}</p>
+            <button>Delete</button>
+          </div>
+        );
+      })}
     </div>
   );
 }
