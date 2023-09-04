eac17b6c616380cb761cd60e9e6106c19392758b
Alvaro0004 <alvarollano04@gmail.com>
2023-09-04 19:27:41 -0400

    Consume-rest_apis: Adding the title and body states variables and handling the add method to prevent the default behavior of submit button

--- Basic Projects/consume-rest-apis/src/components/App.js
+++ Basic Projects/consume-rest-apis/src/components/App.js
@@ -2,7 +2,10 @@ import { useState, useEffect } from 'react';
 
 export default function App() {
   const [posts, setPosts] = useState([]);
+  const [title, setTitle] = useState('');
+  const [body, setBody] = useState('');
 
+  // MANAGE DATA
   useEffect(() => {
     fetch('https://jsonplaceholder.typicode.com/posts?_limit=10')
       .then((response) => response.json())
@@ -15,16 +18,24 @@ export default function App() {
       });
   }, []);
 
+  // HANDLE ADDING MORE POST
+  function handleAdd(e) {
+    e.preventDefault();
+  }
+
   return (
+    // CARD FORM
     <div className="App">
       <div className="form-card">
         <h1>Add a new Post</h1>
         <form>
-          <input type="text" placeholder="Your title" />
-          <textarea placeholder="Your post"></textarea>
-          <button>Save</button>
+          <input type="text" placeholder="Your title" value={title} onChange={(e) => setTitle(e.target.value)} />
+          <textarea placeholder="Your post" value={body} onChange={(e) => setBody(e.target.value)}></textarea>
+          <button onClick={handleAdd}>Add</button>
         </form>
       </div>
+
+      {/* LIST OS POST  */}
       {posts.map((post) => {
         return (
           <div className="card" key={post.id}>
