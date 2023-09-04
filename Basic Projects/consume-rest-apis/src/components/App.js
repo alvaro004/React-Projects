1e6d64dd12ab5827a24ac557cef138ba869bf165
Alvaro0004 <alvarollano04@gmail.com>
2023-09-04 19:38:59 -0400

    Consume-rest_apis: Adding new posts

--- Basic Projects/consume-rest-apis/src/components/App.js
+++ Basic Projects/consume-rest-apis/src/components/App.js
@@ -18,9 +18,34 @@ export default function App() {
       });
   }, []);
 
+  //ADDING MORE POST IN THE API
+  async function addPosts(title, body) {
+    await fetch('https://jsonplaceholder.typicode.com/posts', {
+      method: 'POST',
+      body: JSON.stringify({
+        title: title,
+        body: body,
+        userId: Math.random().toString(36).slice(2),
+      }),
+      headers: {
+        'Content-type': 'application/json; charset=UTF-8',
+      },
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        setPosts((posts) => [data, ...posts]);
+        setTitle('');
+        setBody('');
+      })
+      .catch((err) => {
+        console.log(err.message);
+      });
+  }
+
   // HANDLE ADDING MORE POST
   function handleAdd(e) {
     e.preventDefault();
+    addPosts(title, body);
   }
 
   return (
